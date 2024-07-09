4eea84c2965692c6ee44e94e8c4ad6617743bea1
yjfw <yjfw2024@163.com>
2024-07-09 19:05:12 +0800

    Registration-比赛指南组件开发完毕

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>我要报名</h1>
+    <el-menu
+      default-active="1"
+      mode="horizontal">
+      <el-menu-item index="1">填写作品信息</el-menu-item>
+      <el-menu-item index="2">上传参赛作品</el-menu-item>
+    </el-menu>
   </div>
 </template>
 
