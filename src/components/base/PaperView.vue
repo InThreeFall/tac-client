38550b14c56a0d028d89a1bc9da7deba86440dc4
yjfw <yjfw2024@163.com>
2024-07-10 13:37:56 +0800

    封装组件PaperView

---
+++ src/components/base/PaperView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="background: aliceblue;width: 100%;padding-top: 20px;padding-bottom: 20px;">
+    <div style="background-color: white;width: 80%;margin:0 auto;padding: 20px;">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <el-menu
-      default-active="1"
-      mode="horizontal">
-      <el-menu-item index="1">填写作品信息</el-menu-item>
-      <el-menu-item index="2">上传参赛作品</el-menu-item>
-    </el-menu>
-  </div>
+  <PaperView>
+    <div>
+      <el-menu
+          default-active="1"
+          mode="horizontal">
+        <el-menu-item index="1">填写作品信息</el-menu-item>
+        <el-menu-item index="2">上传参赛作品</el-menu-item>
+      </el-menu>
+    </div>
+  </PaperView>
 </template>
 
-<script>
+<script setup>
+import PaperView from '@/components/base/PaperView.vue'
 </script>
 
 <style scoped>
-
 </style>
